cd078b731eedf0a91d747c631f5dd35cf75045e2
Eduardo Trujillo <129311642+Lalo64GG@users.noreply.github.com>
2025-02-01 21:52:47 -0600

    Refactor: Updated Login module and fixed case-sensitive issues

--- app/src/main/java/com/example/smartenv/Login/data/model/LoginUserRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.smartenv.Login.data.model
-
-data class LoginUserRequest(
-    val email: String,
-    val password: String
-)
--- app/src/main/java/com/example/smartenv/Login/data/model/UserData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.smartenv.Login.data.model
-
-data class UserData(
-val Email: String,
-val Id: Int,
-val Token: String
-)
